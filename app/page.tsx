6d67a824a2559bc1467a3425eb95e2efafd165ac
BeanieMen <jainarjav886@gmail.com>
2024-03-09 22:45:52 +0530

    testi

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,65 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import TestimonialSlider, { Testimonial } from "@/components/ui/carousel-testimonial"
+
 const pacifico = Chakra_Petch({ weight: '400', subsets: ["latin"] });
+
+const testimonials: Testimonial[] = [
+  {
+    quote:
+      "Nostrud tempor sunt fugiat. Dolor in sint dolore labore non occaecat adipisicing Lorem labore ullamco enim excepteur. In fugiat Lorem sit velit id veniam esse eiusmod non ea voluptate cupidatat reprehenderit ullamco dolore. Mollit laborum occaecat aliquip.",
+    name: "Rose Roberson",
+    role: "CEO at Company",
+    imgSrc: "https://i.pravatar.cc/120?img=1",
+  },
+  {
+    quote:
+      "Eiusmod dolor aute ut nulla pariatur officia consequat aute amet exercitation. Culpa consectetur dolor pariatur commodo aliqua amet tempor nisi enim deserunt elit cillum.",
+    name: "Chace Rodgers",
+    role: "CEO at Company",
+    imgSrc: "https://i.pravatar.cc/120?img=10",
+  },
+  {
+    quote:
+      "Id duis velit enim officia ad nisi incididunt magna ex dolor minim deserunt dolor.",
+    name: "Cornelius Sheppard",
+    role: "CEO at Company",
+    imgSrc: "https://i.pravatar.cc/120?img=9",
+  },
+  {
+    quote:
+      "Consectetur voluptate pariatur dolore laboris. Eiusmod dolor aute ut nulla pariatur officia consequat aute amet exercitation.",
+    name: "Chace Rodgers",
+    role: "CEO at Company",
+    imgSrc: "https://i.pravatar.cc/120?img=7",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur voluptate pariatur dolore laboris. Eiusmod dolor aute ut nulla pariatur officia consequat aute amet exercitation.",
+    name: "Cornelius Sheppard",
+    role: "CEO at Company",
+    imgSrc: "https://i.pravatar.cc/120?img=8",
+  },
+  {
+    quote:
+      "Consectetur voluptate pariatur dolore laboris. Eiusmod dolor aute ut nulla pariatur officia consequat aute amet exercitation.",
+    name: "Chace Rodgers",
+    role: "CEO at Company",
+    imgSrc: "https://i.pravatar.cc/120?img=2",
+  },
+  {
+    quote:
+      "Id duis velit enim officia ad nisi incididunt magna ex dolor minim deserunt dolor.",
+    name: "Cornelius Sheppard",
+    role: "CEO at Company",
+    imgSrc: "https://i.pravatar.cc/120?img=3",
+  },
+];
+
 
 export default function Home() {
   return (
@@ -106,6 +160,10 @@ export default function Home() {
             />
           </div>
         </div>
+      </div>
+
+      <div>
+        <TestimonialSlider testimonials={testimonials}/> 
       </div>
     </main>
 
