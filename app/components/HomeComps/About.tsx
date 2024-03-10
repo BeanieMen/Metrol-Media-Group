aa773185c1b7f585230d572b0f31c4609aba1cfc
BeanieMen <jainarjav886@gmail.com>
2024-03-10 19:15:24 +0530

    improve

--- app/components/HomeComps/About.tsx
+++ app/components/HomeComps/About.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function About() {
     return (
@@ -6,12 +6,18 @@ export default function About() {
             <div className="text-white text-center py-20 md:py-20">
                 <div className="text-4xl md:text-6xl font-bold mb-20">About Us</div>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-20 mx-5 md:mx-[7rem]">
-                    <div className="flex-col justify-center my-auto">
+                    <div className="flex flex-col justify-center my-auto">
                         <div className="text-2xl font-bold mb-4">Expertising Media & Marketing since 2019</div>
-                        <ul className=" ml-6 font-light text-lg">
-                            <li>Largest Studio with 50+ Setups in Delhi NCR</li>
-                            <li>100+ successful social media accounts</li>
-                            <li>2.5 Cr+ annual meta ads spent</li>
+                        <ul className="ml-6 lex flex-col gap-y-2 font-light sm:text-sm lg:text-lg flex flex-wrap">
+                            <li className="flex-grow flex-shrink">
+                                Largest Studio with 50+ Setups in Delhi NCR
+                            </li>
+                            <li className="flex-grow flex-shrink">
+                                100+ successful social media accounts
+                            </li>
+                            <li className="flex-grow flex-shrink">
+                                2.5 Cr+ annual meta ads spent
+                            </li>
                         </ul>
                     </div>
                     <div>
@@ -26,5 +32,5 @@ export default function About() {
                 </div>
             </div>
         </>
-    )
+    );
 }
