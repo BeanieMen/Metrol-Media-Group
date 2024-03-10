97f8dc33a65069499592b503e7f819a3e8c69998
BeanieMen <jainarjav886@gmail.com>
2024-03-10 18:53:43 +0530

    improve

--- app/components/HomeComps/Hero.tsx
+++ app/components/HomeComps/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Hero() {
     return <>
-        <div className="bg-black text-white text-5xl md:text-6xl text-center py-40">
+        <div className="bg-black text-white text-5xl md:text-6xl text-center py-20">
             <div className="font-bold mb-4">Elevate Your Brand&apos;s Presence</div>
             <div className="text-2xl md:text-4xl mb-10">
                 Forge ahead in the digital realm with Delhi&apos;s finest marketing mavens.
--- app/components/HomeComps/Services.tsx
+++ app/components/HomeComps/Services.tsx
@@ -9,36 +9,37 @@ import {
 export default function Services() {
     return (
         <>
-            <div className="py-28">
-                <div className="text-white text-center text-4xl md:text-6xl font-bold mb-10">Our Services</div>
+            <div className="py-8 px-4 md:py-20 md:px-0 flex justify-center">
+                <div className="max-w-screen-lg">
+                    <div className="text-white text-center text-4xl md:text-6xl font-bold mb-6 md:mb-10">Our Services</div>
 
-                <div className="grid grid-cols-1  md:grid-cols-2 mx-[5vw] lg:mx-[22.5vw] gap-40">
-                    <Card className="bg-gray-950">
-                        <CardHeader>
-                            <CardTitle className="font-bold text-2xl">CONTENT AND MEDIA MARKETING</CardTitle>
-                            <CardDescription>For Content Creators, Brands, and More</CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            <p className="text-lg font-light">
-                                Elevate your brand&apos;s online presence with our comprehensive Content and Media Marketing services. From engaging social media campaigns to compelling blog posts and captivating video content, we specialize in crafting strategies that resonate with your audience. Let our experts drive traffic, increase engagement, and ultimately boost conversions, helping you stand out in today&apos;s competitive digital landscape.
-                            </p>
-                        </CardContent>
-                    </Card>
+                    <div className="flex flex-col md:flex-row md:justify-center gap-6 lg:gap-40">
+                        <Card className="bg-gray-950 flex-1">
+                            <CardHeader>
+                                <CardTitle className="font-bold text-xl md:text-3xl">CONTENT AND MEDIA MARKETING</CardTitle>
+                                <CardDescription>For Content Creators, Brands, and More</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="text-sm md:text-lg font-light">
+                                    Elevate your brand&apos;s online presence with our comprehensive Content and Media Marketing services. From engaging social media campaigns to compelling blog posts and captivating video content, we specialize in crafting strategies that resonate with your audience. Let our experts drive traffic, increase engagement, and ultimately boost conversions, helping you stand out in today&apos;s competitive digital landscape.
+                                </p>
+                            </CardContent>
+                        </Card>
 
-                    <Card className="bg-gray-950">
-                        <CardHeader>
-                            <CardTitle className="font-bold text-2xl">META ADS</CardTitle>
-                            <CardDescription>For Marketers, Brands, and More</CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            <p className="text-lg font-light">
-                                Elevate your online presence with our cutting-edge Meta Ads service. Our specialized ad campaigns are designed to target your audience effectively across various platforms, maximizing your reach and engagement. Whether you&apos;re a small business or a large corporation, our expert team will tailor strategies to suit your specific goals and budget. Let us help you amplify your digital presence and drive measurable results with our innovative advertising solutions.
-                            </p>
-                        </CardContent>
-                    </Card>
+                        <Card className="bg-gray-950 flex-1 lg:mt-0">
+                            <CardHeader>
+                                <CardTitle className="font-bold text-xl md:text-3xl">META ADS</CardTitle>
+                                <CardDescription>For Marketers, Brands, and More</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="text-sm md:text-lg font-light">
+                                    Elevate your online presence with our cutting-edge Meta Ads service. Our specialized ad campaigns are designed to target your audience effectively across various platforms, maximizing your reach and engagement. Whether you&apos;re a small business or a large corporation, our expert team will tailor strategies to suit your specific goals and budget. Let us help you amplify your digital presence and drive measurable results with our innovative advertising solutions.
+                                </p>
+                            </CardContent>
+                        </Card>
+                    </div>
                 </div>
             </div>
         </>
-
     )
 }
